4ea6117ae4b0fcd9278ba1459892bc9b0e520109
manishroy09 <manishroy2163@gmail.com>
2024-12-18 20:00:49 +0530

    18th dec 2024

--- F1.java
+++ F1.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 class F1{
 
   public void add(){
+    //step1: declare variable
+     System.out.println("========>Enter Addition Function <==========");
     int a,b,c;
-    System.out.println("========> Add Function is called <==========");
+
+     //Input 
+     
+
+    
     Scanner kb=new Scanner(System.in);
     System.out.println(" Enter Value of A : ");
     a=kb.nextInt();
@@ -12,20 +18,37 @@ class F1{
     System.out.println(" Enter Value of B : ");
     b=kb.nextInt();
  
+    // Perform operation 
+
     c=a+b;
     
+    // Print Result 
+
     System.out.println(" Addition : " +c);
+     System.out.println("========>Exit Addition Function<=======");
      }
+     
 
     public static void main(String args[]){
+     System.out.println("Back to main funtion");
+
+      System.out.println("========>Enter Main Function<=======");
+
+
     F1 obj=new F1();
+    
+    // How to access  method of class
+    //objectName.methodName()
 
     obj.add();
+      System.out.println("Back to main funtion");
+
+      
+
     obj.add();
-    obj.add();
-    obj.add();
-    obj.add();
-   
+      System.out.println("========>Exit Main Function<=======");
+
+       
   }
 }
   
